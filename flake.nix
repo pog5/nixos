b85aca79235c659bd0ded8a0ca1949dd497e5e97
Nightly <62222436+pog5@users.noreply.github.com>
2023-06-17 13:12:05 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     prismlauncher.url = "github:PrismLauncher/PrismLauncher";
   };
   outputs = { self, nixpkgs, ... }@attrs: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.nixos-thinkcent = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = attrs;
       modules = [ ./configuration.nix ];
